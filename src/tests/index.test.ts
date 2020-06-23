48d4a0b118f831f809777acbe9222795e19b7be1
ste <ste@MacBook-Air.fritz.box>
2020-06-23 17:35:25 +0200

    Adds tests for /?language&username endpoint
    Tests for:
    - 400
    - 404
    - 200
    - Response body
    
    Nocks the calls to github api in order to not actually ping
    their service for testing purposes.

---
+++ src/tests/index.test.ts
@@ -0,0 +1,48 @@
+import 'chai-http'
+import * as chai from 'chai'
+chai.use(require('chai-http'))
+import request from 'supertest'
+import nock from 'nock'
+
+import app from '..'
+
+describe('API Endpoint(s)', () => {
+	const expect = chai.expect
+	const scope = nock('https://api.github.com')
+
+	describe('GET /?username&language', () => {
+		it('Returns 400 when no parameters in query', async () => {
+			const res = await request(app).get('/').query({})
+			expect(res.status).equals(400)
+		})
+
+		it('Returns empty when none found', async () => {
+				scope.get('/search/users?q=meow+language:something&in:login&type=Users')
+				.reply(200, { items: [] })
+			const res = await request(app).get('/').query({ username: 'meow', language: 'something' })
+			expect(res.status).equals(404)
+		})
+
+		it('Returns single user for ', async () => {
+			scope.get('/search/users?q=test-username+language:python&in:login&type=Users')
+				.reply(200, {
+					items: [{
+						url: 'https://api.github.com/users/alink'
+					}]
+				})
+				.get('/users/alink')
+				.reply(200, {
+					avatar_url: 'something',
+					login: 'test-username',
+					followers: 3,
+					name: 'test-username',
+					url: '/alink'
+				})
+				
+			const res = await request(app).get('/').query({ username: 'test-username', language: 'python' })
+			expect(res.status).equals(200)
+			expect(res.body).haveOwnProperty('users')
+			expect(res.body.users).length(1)
+		})
+	})
+})
