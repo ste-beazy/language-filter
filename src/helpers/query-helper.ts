bcbb6fe07b3cb8d306aeed44f9e71c44c746160f
ste <ste@MacBook-Air.fritz.box>
2020-06-23 10:59:51 +0200

    Formats files for ease of reading
    Reduces vsc tab indent to 2 from 4

--- src/helpers/query-helper.ts
+++ src/helpers/query-helper.ts
@@ -1,19 +1,19 @@
 export const constructQuery = (query: any) => {
-    const { username, language } = query
-    if (!username || !language) return
+	const { username, language } = query
+	if (!username || !language) return
 
-    let languageQuery = ''
-    const isString = typeof language === 'string'
-    if (!isString && language.length > 1) {
-        language.forEach((lang: string, index: number) => {
-            if (index === 0) {
-                languageQuery = `language:${lang}`
-                return
-            }
-            languageQuery += `&language:${lang}`
-        })
-    } else {
-        languageQuery = `language:${language}`
-    }
-    return `${username}+${languageQuery}`
+	let languageQuery = ''
+	const isString = typeof language === 'string'
+	if (!isString && language.length > 1) {
+		language.forEach((lang: string, index: number) => {
+				if (index === 0) {
+						languageQuery = `language:${lang}`
+						return
+				}
+				languageQuery += `&language:${lang}`
+		})
+	} else {
+			languageQuery = `language:${language}`
+	}
+	return `${username}+${languageQuery}`
 }
--- src/index.ts
+++ src/index.ts
@@ -8,34 +8,34 @@ const app = express()
 app.set('view engine', 'pug');
 
 app.get('/authorization/received', async (req, res) => {
-    const { code } = req.query
-    await Api.authenticate(code)
-    res.render('confirmation')
+	const { code } = req.query
+	await Api.authenticate(code)
+	res.render('confirmation')
 })
 
 app.get('/render', (req, res) => {
-    res.render('index', {
-        redirectUri: 'http://localhost:8080/confirmation/received',
-        clientId: Api.clientId
-    })
+	res.render('index', {
+			redirectUri: 'http://localhost:8080/confirmation/received',
+			clientId: Api.clientId
+	})
 })
 
 app.get('/', async (req, res) => {
-    const queryParams = constructQuery(req.query)
-    if (!queryParams) {
-        return res.status(400).send({ error: 'Missing query parameters.' })
-    }
-    try {
-        const users = await Api.fetchUsers(queryParams)
-        if (!users.length) {
-            return res.send({ message: 'Sorry, no users were found that matched your search.' })
-        }
-        res.send({ users })
-    } catch (e) {
-        res.status(400).send({ message: 'Something went wrong', error: {...e} })
-    }
+	const queryParams = constructQuery(req.query)
+	if (!queryParams) {
+			return res.status(400).send({ error: 'Missing query parameters.' })
+	}
+	try {
+			const users = await Api.fetchUsers(queryParams)
+			if (!users.length) {
+					return res.send({ message: 'Sorry, no users were found that matched your search.' })
+			}
+			res.send({ users })
+	} catch (e) {
+			res.status(400).send({ message: 'Something went wrong', error: {...e} })
+	}
 })
 
 app.listen(8080, () => {
-    console.log('we in this')
+	console.log('we in this')
 })
