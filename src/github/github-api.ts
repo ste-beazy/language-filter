3691f8a29d8d979b095d41845d32ea052769bd5b
ste <ste@MacBook-Air.fritz.box>
2020-06-25 12:23:22 +0200

    Refactors Github API, adds props to returned User
    Adds properties createdAt, publicRepos, and restructures
    the response object from getUserByUrl.
    
    Renames baseUrl > base, bc it feels more readable/reasonable.

--- src/github/github-api.ts
+++ src/github/github-api.ts
@@ -4,16 +4,16 @@ import { timeout } from '../helpers/'
 
 class GithubApi {
 	public timeoutLimit: number
-	private baseUrl: string
+	private base: string
 	private headers: Headers
 	constructor() {
 			this.timeoutLimit = 1000
-			this.baseUrl = 'https://api.github.com'
+			this.base = 'https://api.github.com'
 	}
 
 	async fetchUsers(query: string): Promise<any> {
 			try {
-					const res: any = await timeout(this.timeoutLimit, fetch(`${this.baseUrl}/search/users?q=${query}&in:login&type=Users`, {
+					const res: any = await timeout(this.timeoutLimit, fetch(`${this.base}/search/users?q=${query}&in:login&type=Users`, {
 							headers: this.headers
 					}))
 					const { items } = await res.json()
@@ -21,7 +21,7 @@ class GithubApi {
 
 					return Promise.all(items.map((user: any) => this.fetchUserFromUrl(user.url)))    
 			} catch (e) {
-
+				// what could possibly go wrong?
 			}
 	}
 
@@ -29,10 +29,12 @@ class GithubApi {
 			const res = await timeout(this.timeoutLimit, fetch(url, { headers: this.headers }))
 			const user = await res.json()
 			return {
-					avatar_url: user.avatar_url,
-					username: user.login,
-					followers: user.followers,
-					name: user.name ? user.name : null
+				avatarUrl: user.avatar_url,
+				username: user.login,
+				followers: user.followers,
+				name: user.name,
+				publicRepos: user.public_repos,
+				createdAt: user.created_at
 			}
 	}
 }
