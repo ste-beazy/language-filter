64deaa26dd13ae159672075a7baa59420fa2c614
ste <ste@MacBook-Air.fritz.box>
2020-06-25 11:31:27 +0200

    Refactors GitHubApi, removes OAuth and adds timeout
    Adds timeout handler in the event that the request from github
    does not resolve quickly enough.
    
    Cleans up github api by removing OAuth workflow as it seemed to
    be outside project scope.

--- src/github/github-api.ts
+++ src/github/github-api.ts
@@ -1,53 +1,32 @@
 import fetch, { Headers } from 'node-fetch'
 
+import { timeout } from '../helpers/'
+
 class GithubApi {
-	public clientId: string
-	private clientSecret: string
+	public timeoutLimit: number
 	private baseUrl: string
 	private headers: Headers
 	constructor() {
-			this.clientId = 'swerve'
-			this.clientSecret = 'onem'
+			this.timeoutLimit = 1000
 			this.baseUrl = 'https://api.github.com'
-	}
-
-	async authenticate (code: any) {
-			try {
-					// TODO: Add wrapper function to handle res.json()
-					console.log('before access token')
-					const { access_token } = await fetch(`https://github.com/login/oauth/access_token?client_id=${this.clientId}&client_secret=${this.clientSecret}&code=${code}`, {
-							method: "POST",
-							headers: {
-									"Accept": "application/json"
-							}
-					}).then((res) => res.json())
-					const headers = new Headers()
-					headers.append('Authorization', 'token ' + access_token);
-					headers.append("X-GitHub-Media-Type", "github.v3")
-					headers.append('Accept', 'application/json')
-					this.headers = headers
-			} catch (e) {
-					// TODO: Add proper error handling
-					console.log({ e })
-			}
 	}
 
 	async fetchUsers(query: string): Promise<any> {
 			try {
-					const res: any = await fetch(`${this.baseUrl}/search/users?q=${query}&in:login&type=Users`, {
+					const res: any = await timeout(this.timeoutLimit, fetch(`${this.baseUrl}/search/users?q=${query}&in:login&type=Users`, {
 							headers: this.headers
-					})
+					}))
 					const { items } = await res.json()
 					if (!items.length) return []
 
-					return Promise.all(items.map((user: any) => this.fetchUser(user.url)))    
+					return Promise.all(items.map((user: any) => this.fetchUserFromUrl(user.url)))    
 			} catch (e) {
-					console.log({ e })
+
 			}
 	}
 
-	async fetchUser(url: string) {
-			const res = await fetch(url, { headers: this.headers })
+	async fetchUserFromUrl(url: string) {
+			const res = await timeout(this.timeoutLimit, fetch(url, { headers: this.headers }))
 			const user = await res.json()
 			return {
 					avatar_url: user.avatar_url,
